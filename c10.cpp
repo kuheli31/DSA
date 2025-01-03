7c10324cb3f15dd0da2bafab0dfea45251de83ec
kuheli31 <133209155+kuheli31@users.noreply.github.com>
2025-01-03 16:26:43 +0530

    Files changed

--- c10.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int n,m;
-	printf("Enter the number=");
-	scanf("%d",&n);
-	m=n/100;
-	printf("Remaining number after deleting last two digit number=%d",m);
-}
--- c8.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int n,rem;
-	printf("Enter the number=");
-	scanf("%d",&n);
-	rem=n%10;
-	printf("Last digit of number=%d",rem);
-}
